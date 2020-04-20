78be2c1fd158e41352f375c81be4ec7c24ad028c
richrosenthal <rrosenthal2013@gmail.com>
2020-04-20 05:44:40 -0400

    initial commit

---
+++ wiki.rb
@@ -0,0 +1,29 @@
+require "sinatra"
+
+# set :bind, "0.0.0.0"
+
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
+get "/" do
+  erb :welcome
+end
+
+get "/:title" do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :show
+end
+
+get "/new" do
+
+ end
